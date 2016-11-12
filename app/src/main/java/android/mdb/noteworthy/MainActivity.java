728f8e0b559dffa7414eeddc73c91e7a53b2fdba
Kedar Thakkar <kedarthakkar@airbears2-10-142-96-238.airbears2.1918.berkeley.edu>
2016-11-11 19:13:45 -0800

    Song Feed Layout File

--- app/src/main/java/android/mdb/noteworthy/MainActivity.java
+++ app/src/main/java/android/mdb/noteworthy/MainActivity.java
@@ -1,13 +1,48 @@
 package android.mdb.noteworthy;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    SongsAdapter songAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.song_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<Song> songList = new ArrayList<Song>();
+
+        Song song1 = new Song("h", "Chorale and Shaker Rhythm", "by Aayush Tyagi", "h", "Length: 3:30");
+        Song song2 = new Song("h", "Chorale and Shaker Rhythm", "by Aayush Tyagi", "h", "Length: 3:30");
+        Song song3 = new Song("h", "Chorale and Shaker Rhythm", "by Aayush Tyagi", "h", "Length: 3:30");
+        Song song4 = new Song("h", "Chorale and Shaker Rhythm", "by Aayush Tyagi", "h", "Length: 3:30");
+        Song song5 = new Song("h", "Chorale and Shaker Rhythm", "by Aayush Tyagi", "h", "Length: 3:30");
+        Song song6 = new Song("h", "Chorale and Shaker Rhythm", "by Aayush Tyagi", "h", "Length: 3:30");
+        Song song7 = new Song("h", "Chorale and Shaker Rhythm", "by Aayush Tyagi", "h", "Length: 3:30");
+
+        songList.add(song1);
+        songList.add(song2);
+        songList.add(song3);
+        songList.add(song4);
+        songList.add(song5);
+        songList.add(song6);
+        songList.add(song7);
+
+        songAdapter = new SongsAdapter(getApplicationContext(), songList);
+
+        recyclerView.setAdapter(songAdapter);
+
     }
 }
